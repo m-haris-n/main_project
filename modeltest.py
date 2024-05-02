d8bd466654f9f87b743fac136503e8e17dcb3701
mharisn <m.haris.nabeel@gmail.com>
2024-05-02 13:43:11 +0500

    model test added

--- modeltest.py
+++ modeltest.py
@@ -1,2 +1,24 @@
-from ultralytics import YOLO
 import cv2
+from ultralytics import YOLO
+
+
+model = YOLO("model.pt")
+results = model.predict(
+    source="0",
+    show=True,
+    stream=True,
+)
+
+flag = True
+detected_doors = []
+detected_stairs = []
+
+while flag:
+
+    for res in results:
+        boxes = res.boxes
+        print(f"\n\n****BOX****\n")
+
+        for box in boxes:
+            print(f"{box}")
+            x1, y1, x2, y2 = [int(x) for x in box.xyxy[0]]
